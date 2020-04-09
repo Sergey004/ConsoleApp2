02341c0cb97e8e639665301674841de21e41ad70
Sergey Grigoriev <sergey00475@gmail.com>
2020-04-09 11:19:41 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            ShowMessage method = Show;
+
+
+            student.Move(7, method);
+
+        }
+        static void Show(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public delegate void ShowMessage(string message);
+        
+
+
+        public class Student
+        {
+            public void Move(int distance, ShowMessage method)
+            {
+                for (int i = 0; i <= distance; i++)
+                {
+                    Thread.Sleep(1000);
+                    method(string.Format("Идеи перемещение... Пройдено километров {0}", i));
+                }
+            }
+
+        }
+    }
+}       
